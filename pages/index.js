dd27f38330ee5cee37a5c47ddc2c19cc3a15e258
Valerii Vitenko <valero2313@gmail.com>
2022-05-10 23:13:32 +0300

    added custom API for own social links on home page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import Head from 'next/head';
 import Heading from '../components/Heading';
+import Socials from '../components/Socials';
 import s from '../styles/Home.module.scss';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const response = await fetch(`${process.env.API_HOST}/socials`);
+  const data = await response.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { socials: data },
+  };
+};
+
+export default function Home({ socials }) {
   return (
     <div className={s.wrapper}>
       <Head>
         <title>Home Page</title>
       </Head>
       <Heading text="Hello from react NextJS" />
+      <Socials socials={socials} />
     </div>
   );
 }
